a6421fd1d468ee2865afc2c5018679a04d1d26ac
Lennon da Cunha <lennon.palmerio@gmail.com>
2020-01-14 17:00:36 -0300

    init commit

---
+++ src/application/Main.java
@@ -0,0 +1,46 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	
+	private static double x;
+	private static double y;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			VBox root = (VBox)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			scene.setFill(Color.TRANSPARENT);
+			primaryStage.initStyle(StageStyle.TRANSPARENT);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+			root.setOnMousePressed((press)->{
+				x = press.getSceneX();
+				y = press.getSceneY();
+			});
+			
+			root.setOnMouseDragged((drag)->{
+				primaryStage.setX(drag.getScreenX() - x);
+				primaryStage.setY(drag.getScreenY() - y);
+			});
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
