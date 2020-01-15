3cd399855b3094ddbd2f4f8e38ab17133f32b9cb
Lennon da Cunha <lennon.palmerio@gmail.com>
2020-01-15 16:56:25 -0300

    add cedulas

---
+++ src/application/CedulaController.java
@@ -0,0 +1,18 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class CedulaController {
+
+    @FXML
+    private TextField txfQuantidade;
+
+    @FXML
+    private Text textNOmeCedula;
+
+    @FXML
+    private Text textTotalCedula;
+
+}
--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -1,5 +1,53 @@
 package application;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
 public class SampleController {
-	
+
+    @FXML
+    private Button btnADD;
+
+    @FXML
+    private Button btnPDF;
+
+    @FXML
+    private Button btnFechar;
+
+    @FXML
+    private VBox vbCedulas;
+
+    @FXML
+    void fechar() {
+    	System.exit(0);
+    }
+
+    @FXML
+    void gerarPDF() {
+    	//TODO GERAR UM PDF
+    	System.out.println("Gerar PDF");
+    }
+
+    @FXML
+    void addCedula() {
+		try {
+			VBox root = (VBox)FXMLLoader.load(getClass().getResource("Cedula.fxml"));
+			vbCedulas.getChildren().add(root);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  	
+    }
+
+
+
 }
+
